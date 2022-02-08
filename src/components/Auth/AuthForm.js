d2082a67e54f26688674dd2b9e279616eeaa3f75
kimminkyu <kmk50009@naver.com>
2022-02-09 00:15:17 +0900

    Add Redirect

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,9 +1,10 @@
 import { useState, useRef, useContext } from "react";
 import AuthContext from "../../store/auth-context";
-
+import { useHistory } from "react-router-dom";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -60,6 +61,8 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken);
+        history.replace('/');
+
       })
       .catch((err) => {
         alert(err.message);
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,8 +1,10 @@
 import classes from "./ProfileForm.module.css";
 import { useRef, useContext } from "react";
 import AuthContext from "../../store/auth-context";
+import { useHistory } from 'react-router-dom';
 
 const ProfileForm = () => {
+  const history = useHistory();
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -28,8 +30,8 @@ const ProfileForm = () => {
       }
     ).then((res) => {
       // assumption: Always succeeds!
-    
-      
+      history.replace('/')
+
     });
   };
   return (
